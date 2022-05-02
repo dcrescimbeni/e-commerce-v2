93d184e79026f070e81a053c74bd386550c0fe47
Dino <crescimbenidino@gmail.com>
2022-05-02 16:23:16 -0300

    Changes to db connection data

--- api/config/db.js
+++ api/config/db.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const { NODE_ENV, DATABASE_URL } = process.env;
 
-const databaseName = NODE_ENV === 'test' ? 'ecommercetest' : 'ecommercev2';
+// const databaseName = NODE_ENV === 'test' ? 'ecommercetest' : 'ecommercev2';
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(`${DATABASE_URL}/${databaseName}`, {
-  logging: false,
+const db = new Sequelize(`${DATABASE_URL}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = db;

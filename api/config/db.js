84857c9c8ef96d01d6937e495f4dc62f644f440e
Dino <crescimbenidino@gmail.com>
2022-05-02 16:16:32 -0300

    Added env to DB config

--- api/config/db.js
+++ api/config/db.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
-const { NODE_ENV } = process.env;
+const { NODE_ENV, DATABASE_URL } = process.env;
+
 const databaseName = NODE_ENV === 'test' ? 'ecommercetest' : 'ecommercev2';
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(
-  `postgres://postgres:postgres@localhost:5432/${databaseName}`,
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(`${DATABASE_URL}/${databaseName}`, {
+  logging: false,
+});
 
 module.exports = db;
